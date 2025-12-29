e7021822dd30fafd9ad788485a67aa4a482876c7
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2025-12-29 11:01:53 +0700

    chore(SN-1): build source code

--- src/modules/skin-analysis/skin-analysis.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SkinAnalysisService } from './skin-analysis.service';
-import { SkinAnalysisController } from './skin-analysis.controller';
-
-@Module({
-  controllers: [SkinAnalysisController],
-  providers: [SkinAnalysisService],
-})
-export class SkinAnalysisModule {}
