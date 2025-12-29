c9d4ba8832cdd3540689090b495e0b354b923a36
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2025-12-29 11:01:53 +0700

    chore(SN-1): build source code

--- src/modules/skin-analysis/dto/update-skin-analysis.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSkinAnalysisDto } from './create-skin-analysis.dto';
-
-export class UpdateSkinAnalysisDto extends PartialType(CreateSkinAnalysisDto) {}
